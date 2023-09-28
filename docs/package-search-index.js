163cb302727a6330a2041aedbe6f69b69890cd83
Me <you@example.com>
2023-09-28 10:11:27 +0200

    adding documentation

---
+++ docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.app.microservice"},{"l":"com.app.microservice.controller"},{"l":"com.app.microservice.filter"},{"l":"com.app.microservice.model"},{"l":"com.app.microservice.service"}];updateSearchResults();
